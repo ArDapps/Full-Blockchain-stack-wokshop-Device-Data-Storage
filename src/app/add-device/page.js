4e7c61a8b3a0df9ada8740ecf9ce109d6cffb597
دابس بالعربي <arabicdapps@gmail.com>
2024-10-08 12:11:53 +0300

    add pages

---
+++ src/app/add-device/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddDevice = () => {
+  return <div>welcome to add Device</div>;
+};
+
+export default AddDevice;
---
+++ src/app/my-profile/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyProfile = () => {
+  return <div>Welcome to my profile</div>;
+};
+
+export default MyProfile;
